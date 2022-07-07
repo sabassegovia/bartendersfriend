ce6b2dbc3cc59804ef6f13085f402e5863844cbb
Sabas Segovia <sasegovia@ucdavis.edu>
2022-07-07 15:19:28 -0700

    feat: theming complete, favicon complete

--- myprojects/relief-pitcher-v2/pages/_app.js
+++ myprojects/relief-pitcher-v2/pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import { ThemeProvider } from 'theme-ui'
+import theme from '../theme'
+import Nav from '../src/components/nav'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+        <Nav />
+        <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
-export default MyApp
+export default App;
+
+// import '../styles/globals.css'
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+
+
