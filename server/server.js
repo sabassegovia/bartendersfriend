f003cedaebfa52b72696b0e65723d648072059bd
Sabas Segovia <sasegovia@ucdavis.edu>
2022-08-01 15:22:50 -0700

    feat: server/db set up connection established

--- server/server.js
+++ server/server.js
@@ -1,11 +1,33 @@
-// import modules
+const express = require('express'); // express - to manage servers and routes
+require('dotenv').config();
+const mongoose = require('mongoose'); // for database
+const morgan = require('morgan'); // morgan - logs request, helpful for debugging
+const cors = require('cors'); // cors - for communicatinf front-back end
+const PORT = process.env.PORT || 8080;
 
 // app
+const app = express();
 
 // db
+mongoose.connect(process.env.MONGO_URI, {
+  useUnifiedTopology:true,
+  useNewUrlParser: true,
+})
+  .then(() => {
+  console.log('DB CONNECTED')
+  })
+  .catch((err) => {
+    console.log('DB CONNECTION ERROR', err);
+})
 
 // middleware
+app.use(morgan('dev'));
+app.use(cors({ origin: true, credentials: true }));
 
 // routes
 
-// port
+
+// listener
+const server = app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
